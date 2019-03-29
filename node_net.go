ffb979a5ca6465a5de5112b0d401c52e05e77435
Myles Horton <myles@getlantern.org>
2019-03-29 16:08:00 -0700

    ignore touch bar address

--- node_net.go
+++ node_net.go
@@ -6,9 +6,15 @@
 
 package uuid
 
-import "net"
+import (
+	"bytes"
+	"net"
+	"runtime"
+)
 
 var interfaces []net.Interface // cached list of interfaces
+
+var touchbarAddr = touchbarAddress()
 
 // getHardwareInterface returns the name and hardware address of interface name.
 // If name is "" then the name and hardware address of one of the system's
@@ -26,8 +32,24 @@ func getHardwareInterface(name string) (string, []byte) {
 	}
 	for _, ifs := range interfaces {
 		if len(ifs.HardwareAddr) >= 6 && (name == "" || name == ifs.Name) {
+			if runtime.GOOS == "darwin" {
+				if bytes.Equal(ifs.HardwareAddr, touchbarAddr) {
+					continue
+				}
+			}
 			return ifs.Name, ifs.HardwareAddr
 		}
 	}
 	return "", nil
+}
+
+// The touchbar address somehow frequently appears as the default network address
+// on OSX. This ignores it.
+func touchbarAddress() []byte {
+	touchbar, err := net.ParseMAC("ac:de:48:00:11:22")
+	if err != nil {
+		// This should never happen.
+		return make([]byte, 0)
+	}
+	return touchbar
 }
